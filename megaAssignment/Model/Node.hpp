0076fa15f9976c22624b551011648426ea8fd5b0
inperium <ibow9416@740s-pl-111.local>
2017-02-13 12:58:10 -0700

    Created the generic type

--- megaAssignment/Model/Node.hpp
+++ megaAssignment/Model/Node.hpp
@@ -26,4 +26,47 @@ public:
     Node<Type> * getNodePointer();
 };
 
+/*
+Implementation section
+*/
+
+template <class Type>
+Node<Type> :: Node()
+{
+    //Explicitally DO NOT instantiate the nodData data member
+    this->nodePointer = nullptr;
+}
+    
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->nodeData = data;
+    this->nodePointer = nullptr;
+}
+    
+template <class Type>
+Node<Type> :: Node(Type data, Node<Type> * next)
+{
+    this->nodeData = data;
+    this->nodePointer = nullptr;
+}
+
+template <class Type>
+void Node<Type> :: setNodeData(Type data)
+{
+    return nodeData = mega;
+}
+    
+template <class Type>
+void Node<Type> :: setNodePointer(Node<Type> Pointer)
+{
+    return this->nodePointer;
+}
+    
+template <class Type>
+Node<Type> * Node<Type> :: getNodePointer()
+{
+    return this->nodePointer;
+}
+    
 #endif /* Node_h */
