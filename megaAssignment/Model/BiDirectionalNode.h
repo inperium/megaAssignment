e035ad24729ef0b4672acac6ee1cdb2c1986487a
inperium <ibow9416@740s-pl-111.local>
2017-02-27 13:45:34 -0700

    Added Bidirectional node

---
+++ megaAssignment/Model/BiDirectionalNode.h
@@ -0,0 +1,18 @@
+//
+//  BiDirectionalNode.h
+//  megaAssignment
+//
+//  Created by Bowen, Isaac on 2/27/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#ifndef BiDirectionalNode_h
+#define BiDirectionalNode_h
+
+template <class Type>
+class BiDirectionalNode : public Node<Type>
+{
+    
+}
+
+#endif /* BiDirectionalNode_h */
