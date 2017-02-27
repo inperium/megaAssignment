3d23d96f1bd897f3180f250692cf3327e1edf583
inperium <ibow9416@740s-pl-111.local>
2017-02-27 13:50:00 -0700

    Wrote stuff

--- megaAssignment/Model/BiDirectionalNode.h
+++ megaAssignment/Model/BiDirectionalNode.h
@@ -12,6 +12,25 @@
 template <class Type>
 class BiDirectionalNode : public Node<Type>
 {
+private:
+    BiDirectionalNode<Type> * previous;
+    BiDirectionalNode<Type> * next;
+    Type Data;
+public:
+    BiDirectioanlNode();
+    BiDirectionalNode(Type Data);
+    BiDirectionalNode(Type data, BiDirectionalNode<Type * previous, BiDirectionalNode<Type> * next);
+}
+
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode() : Node()
+{
+    
+}
+
+template <class Type>
+BiDirectionalNode<Type> :: BidirectionalNode(Type dta) : Node<Type>(data)
+{
     
 }
 
