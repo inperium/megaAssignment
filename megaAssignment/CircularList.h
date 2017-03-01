94e33dc994040e76e99e0ea5bc8cde5136c284b8
inperium <ibow9416@740s-pl-111.local>
2017-03-01 13:41:59 -0700

    lel

---
+++ megaAssignment/CircularList.h
@@ -0,0 +1,13 @@
+//
+//  CircularList.h
+//  megaAssignment
+//
+//  Created by Bowen, Isaac on 3/1/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#ifndef CircularList_h
+#define CircularList_h
+
+
+#endif /* CircularList_h */
---
+++ megaAssignment/Model/CircularList.h
@@ -0,0 +1,31 @@
+//
+//  CircularList.h
+//  megaAssignment
+//
+//  Created by Bowen, Isaac on 3/1/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#ifndef CircularList_h
+#define CircularList_h
+
+template <class Type>
+class CircularList : DoublyLinkedList<Type>
+{
+private:
+
+public:
+    CircularList();
+    ~Circularlist();
+    
+    void add(Type data);
+    Type remove(int index):
+};
+
+template <class Type>
+CircularList<Type> :: CircluarList() : DoublyLinkedList()
+{
+    
+}
+
+#endif /* CircularList_h */
