ca532d2cd4b05d9fbb75c702732635a8944824df
inperium <ibow9416@740s-pl-111.local>
2017-04-21 12:11:11 -0600

    Working on all tree

--- megaAssignment/Model/AVLTree.h
+++ megaAssignment/Model/AVLTree.h
@@ -38,5 +38,93 @@ public:
     void remove(Type value);
 }
 
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinarySearchtreeNode<Type> * node)
+{
+    int balance;
+    int leftHeight = this->calculateHeight(node->getLeftChild());
+    int rightHeight = this->calculateHeight(node->getRightChild());
+    balance = leftHeight-rightHeight;
+    return balance;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: leftRotation (BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getLeftChild();
+    
+    parent->setLeftChild(changedNode->getRightChild());
+    
+    changedNode->setRightChild(Parent);
+                         
+    return changedNode;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: rightRotation (BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getRightChild();
+        
+    parent->setRightChild(changedNode->getRightChild());
+                             
+    changedNode->setLeftChild(Parent);
+                             
+    return changedNode;
+}
+    
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: rightLeftRotation (BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getRightChild();
+        
+    parent->setRightChild(leftRotation(changedNode));
+                              
+    return rightRotation(parent);
+}
+
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: leftRightRotation (BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getLeftChild();
+    
+    parent->setLeftChild(rightRotation(changedNode));
+    
+    return leftRotation(parent);
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVL<Type> :: balanceSubTree(BinarySearchTreeNode<Type> * parent)
+{
+    int balanceFactor = heightDifference(parent);
+    
+    if(balanceFactor > 1)
+    {
+        if(heightdifference(parent->getLeftchild()) > 0)
+        {
+            parent = leftRotation(parent);
+        }
+        else
+        {
+            parent = leftRightRotation(parent);
+        }
+    }
+    else if(balanceFactor < -1)
+    {
+        if(heightDifference(parent->getRightChild()) > 0)
+        {
+            parent = rightLeftRotation(parent);
+        }
+        else
+        {
+            parent = rightRotation(parent);
+        }
+    }
+    return parent;
+}
 
 #endif /* AVLTree_h */
