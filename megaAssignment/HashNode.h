c9b822a637c8f69ea617b0fe0fe6a59e15858556
inperium <ibow9416@740s-pl-111.local>
2017-05-03 12:31:53 -0600

    Added Hash Node

---
+++ megaAssignment/HashNode.h
@@ -0,0 +1,59 @@
+//
+//  HashNode.h
+//  megaAssignment
+//
+//  Created by Bowen, Isaac on 5/3/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#ifndef HashNode_h
+#define HashNode_h
+
+template <class Type>
+class HashNode
+{
+private:
+    Type data;
+    long key;
+public:
+    HashNode();
+    HashNode(Type, data);
+    
+    Type getData();
+    long getKey() const;
+    void setData(Type data);
+};
+
+template <class Type>
+HashNode<Type> :: HashNode()
+{
+    this->key = rand();
+}
+
+template <class Type>
+HashNode<Type> :: HashNode(Type, data)
+{
+    this->key = rand();
+    this->data = data;
+}
+
+template <class Type>
+long HashNode<Type> :: getKey() const
+{
+    return this->key;
+}
+
+template <class Type>
+Type HashNode<Type> :: getData()
+{
+    return this->data;
+}
+
+template <class Type>
+void HashNode<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+#endif /* HashNode_h */
+
+#endif /* HashNode_h */
